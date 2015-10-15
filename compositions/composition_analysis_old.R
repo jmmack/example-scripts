ebc382d85eb891e970b31750c12a947f40c2f667
mmacklai <jean.macklaim@gmail.com>
2015-10-15 18:03:55 -0400

    update compositions

--- compositions/composition_analysis_old.R
+++ compositions/composition_analysis_old.R
@@ -1,7 +1,6 @@
 #22Sept2015
 # Jean Macklaim
 # based on code from GG
-# Samples must be ROWS, so will have to transpose if you are using a QIIME-like OTU table
 
 #--------------------------------------------------------------------------------------------------
 # Install packages
@@ -33,30 +32,13 @@ d.0 <- data.frame(d.1[which(apply(d.1, 1, function(x){sum(x)}) > count),])
 # from zCompositions
 # replace 0 with a Bayesian estimate of 0
 # this is proportions, to get counts use 'output="counts"'
-# samples must be by rows so use t()
+d.bf <- cmultRepl(d.0, label=0, method="CZM")
 
-d.bf <- cmultRepl(t(d.0), label=0, method="CZM")
+# from compositions
+# convert to a clr object with acomp, but it is an S3 object
 
-
-#transform data to CLR
-# the apply function rotates the data when by row, so turn it back to samples by row
-
-d.n0.clr <- t(apply(d, 1, function(x){log2(x) - mean(log2(x))}))
-
-
-# generate the PCA object
-pcx <- prcomp(d.n0.clr)
-
-
-#Jean: set this up to color the biplots
-coloredBiplot(pcx,col=rgb(0,0,0,0.3),cex=c(0.8,0.4), xlabs.col=conds$cond, var.axes=F, arrow.len=0.05)
-
-
-
-#---------------------------------
-# Ignore this down
-
-
+bi <- acomp(d.bf)
+pcx <- princomp(bi)
 
 biplot(pcx, cex=c(0.2,0.6))				#standard plot
 biplot(pcx, cex=c(0.2,0.6), scale=0)	#scale by variables instead of samples
