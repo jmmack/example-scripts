e6ea3116638a5f90ef7d87f130adf7e1e10ea848
Jean Megan Macklaim <jean.macklaim@gmail.com>
2016-04-14 11:48:44 -0400

    Update parse_file_name.sh

--- Bash-shell/parse_file_name.sh
+++ Bash-shell/parse_file_name.sh
@@ -1,12 +1,18 @@
 #Get a list of filenames from a directory
 #Parse out the basename
 
+e.g. A01_2016_R1.fastq.gz
+
 for f in $DATA_DIR/*_R1.fastq.gz; do
   #	echo "$f"
 	B=`basename $f`
   #	echo "basename: $B"
+  #basename: A01_2016_R1.fastq.gz
+
+  #Split on underscore, and get the first field
 	NAME=`echo $B | cut -d "_" -f1`
   #	echo "name: $NAME"
+  #name: A01
 done
 
 #See dirname also
