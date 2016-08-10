397496670f29700d25d77c1556c4c360dd56e950
Jean Megan Macklaim <jean.macklaim@gmail.com>
2016-08-10 16:05:35 -0400

    Update example_compositions_biplot.R

--- CoDa/example_compositions_biplot.R
+++ CoDa/example_compositions_biplot.R
@@ -88,18 +88,21 @@ points <- c(rep(".", length(dimnames(r.pcx$rotation)[[1]])))
 #samples <- c(rep("o", length(dimnames(r.pcx$x)[[1]])))
 
 #color and text size for labels and points (vector of 2)
-# First value in each case is for samples, and the second value for eatures
+# First value in each case is for samples, and the second value for features
 col=c("black",rgb(1,0,0,0.2))
 c=c(0.8, 2)
 
-#Change the name of your plot here
-pdf("test.pdf")
+# scale = 0 will scale by samples, and scale=1 will scale by features
+# var.axes controls the arrows to the features
+# r.pcx$x will be your samples
+#r.pcx$rotation will be your features
+
 biplot(r.pcx, cex=c, col=col, var.axes=F,
     xlab=paste("PC1: ", round(sum(r.pcx$sdev[1]^2)/r.mvar, 3)),
     ylab=paste("PC2: ", round(sum(r.pcx$sdev[2]^2)/r.mvar, 3)),
     scale=0, ylabs=points
 )
-dev.off()
+
 
 # You should also consider making a dendrogram of samples:
 https://github.com/mmacklai/example-scripts/blob/master/R/heatmap_dendrograms.R
@@ -109,4 +112,32 @@ plot(hc)
 #line up labels
 plot(hc, hang=-1)
 
+#-----------------------------------------------
+# Coloring your biplot
 
+## Color by SAMPLE
+# You need to use coloredBiplot
+
+# This makes a vector with 5 red and 5 blue (for example)
+# The length of this vector needs to match the number of rows in r.pcx$x
+sc<-c(rep("red", 5), rep("blue", 5))
+
+coloredBiplot(r.pcx, cex=c, col=col, xlabs.col=sc, var.axes=T,
+    xlab=paste("PC1: ", round(sum(pc.clr$sdev[1]^2)/d.mvar, 3)),
+    ylab=paste("PC2: ", round(sum(pc.clr$sdev[2]^2)/d.mvar, 3)), scale=0)
+
+## Color by FEATURE
+# See the phi script for an automated coloring by cluster size
+
+#biplot(pc.clr, cex=c(0.6,0.4), col=c("black", rgb(1,0,0,0.3)))
+c<-c(1,0.5)
+col<-c("black", rgb(0,0,0,0.1))
+
+# Make a "blank" plot by plotting only the alpha transparency
+biplot(pc.clr, cex=c, col=col, var.axes=F,
+    xlab=paste("PC1: ", round(sum(pc.clr$sdev[1]^2)/d.mvar, 3)),
+    ylab=paste("PC2: ", round(sum(pc.clr$sdev[2]^2)/d.mvar, 3)), scale=0)
+
+# Plotting the first 3 rows (features) in red, and the last (4th feature) in blue
+points(pc.clr$rotation[1:3,1], pc.clr$rotation[1:3,2], col="red")
+points(pc.clr$rotation[4,1], pc.clr$rotation[4,2], col="blue")
