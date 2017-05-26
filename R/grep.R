f919cac88f7cba2611fb31f773c8adac7d3737af
Jean Megan Macklaim <jean.macklaim@gmail.com>
2017-05-26 13:43:25 -0400

    Update grep.R

--- R/grep.R
+++ R/grep.R
@@ -1,4 +1,9 @@
 grep("F6-",colnames(de), value=T)
 #returns the column names matching "F6-"
+#default (value=f) returns the column number, and value=T returns the column name
 
-#default (value=f) returns the column number
+#get the columns from the dataframe
+de[,grep("F6-",colnames(de), value=T)]
+
+#Grep for two patterns (OR)
+grep("F6-|F7-", colnames(de), value=T)
